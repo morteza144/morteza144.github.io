c4197a152e6e2098c4657af75b62b3ad7e5e72ed
morteza <49487722+morteza144@users.noreply.github.com>
2022-06-06 08:18:55 +0430

    Add files via upload

---
+++ js/about.3398e43c.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkvuecli_one"]=self["webpackChunkvuecli_one"]||[]).push([[443],{7381:function(n,e,u){u.r(e),u.d(e,{default:function(){return f}});var t=u(3396);const a={class:"about"},c=(0,t._)("h1",null,"This is an about page",-1),r=[c];function s(n,e){return(0,t.wg)(),(0,t.iD)("div",a,r)}var i=u(89);const o={},l=(0,i.Z)(o,[["render",s]]);var f=l}}]);
+//# sourceMappingURL=about.3398e43c.js.map
